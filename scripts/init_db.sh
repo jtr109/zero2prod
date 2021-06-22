efb5855a0aa347c729eb1e13b45170e060e30abe
Ryan Li <conbas2019@gmail.com>
2021-06-22 08:04:51 +0800

    feat: add migrations

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -11,24 +11,26 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to'5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Launch postgres using Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER}\
-    -e POSTGRES_PASSWORD=${DB_PASSWORD}\
-    -e POSTGRES_DB=${DB_NAME}\
-    -p"${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
-    # ^ Increased maximum number of connections for testing purposes
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    # Launch postgres using Docker
+    docker run \
+        -e POSTGRES_USER=${DB_USER}\
+        -e POSTGRES_PASSWORD=${DB_PASSWORD}\
+        -e POSTGRES_DB=${DB_NAME}\
+        -p"${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+        # ^ Increased maximum number of connections for testing purposes
+fi
 
 # Keep pinging Postgres until it's ready to accept commands
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+until sqlx database create; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+sqlx migrate run
 
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
-sqlx database create
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
